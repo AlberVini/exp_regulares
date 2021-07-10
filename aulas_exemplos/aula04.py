845ef947b3c29d02f15f27a0f14cc4eeb039d0ec
AlberVini <viiniciusalberto@gmail.com>
2021-07-09 23:11:18 -0300

    aula04

---
+++ aulas_exemplos/aula04.py
@@ -0,0 +1,10 @@
+# greedy e non-greedy or lazy
+
+import re
+
+texto = '''
+<p>Something</p> <p>Qualquer coisa</p> <p>Frase 3 </p> <div>Frase 4 </div> 
+'''
+
+print(re.findall(r'<[pdiv]{1,3}>.*<\/[divp]{1,3}>', texto)) # greedy
+print(re.findall(r'<[pdiv]{1,3}>.*?<\/[divp]{1,3}>', texto)[1]) # non-greedy
