824cd87d3b1130deb267341f9015dfeffe39b722
AlberVini <viiniciusalberto@gmail.com>
2021-07-12 03:59:58 -0300

    aula_07

---
+++ aulas_exemplos/aula07.2.py
@@ -0,0 +1,19 @@
+# flags
+# re.I | re.IGNORECASE
+# re.A | re.ASCII tabela ASCII
+# re.M | re.MULTILINE -> auxilia na utilização do ^$ em uma expressão
+# re.S | re.DOTALL 
+
+from enum import Flag
+import re
+
+texto = '''
+123.435.764-77 ABC
+197.744.069-99
+777.444.222.00 DEF
+'''
+print(re.findall(r'^\d{3}.\d{3}.\d{3}-\d{2}$', texto, flags=re.M))
+
+texto2 = 'Tuplas são imutáveis, se quiser \n um tipo de conjunto mutavel, utilize listas'
+
+print(re.findall(r'^t.*s$', texto2, flags=re.I | re.S))
---
+++ aulas_exemplos/aula07.py
@@ -0,0 +1,36 @@
+# shorthand
+# \w para encontrar -> [a-zA-Z0-9À-ú] 
+# \W negação [^a-zA-Z0-9À-ú]
+# \d para encontrar só números -> [0-9]
+# \D negação [^0-9]
+# \s [\r\n\f\t]
+# \S negação, bom para eliminar espaços de strings
+# \b borda o extremidade de uma palavra
+# \b para encontrar uma palavra com determinado número de caracteres
+# \B estranho mas tem a função de não encontrar uma borda
+
+import re
+
+texto = '''
+João trouxe    flores para sua amada namorada em 10 de janeiro de 1970,
+Maria era o nome dela.
+Foi um ano excelente na vida de joão. Teve_ALGO 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
+domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
+pão de queijo.
+Não canso de ouvir a Maria:
+"Joooooooooãooooooo, o café tá prontinho aqui. Veeemm"!
+'''
+
+# print(re.findall(r'[a-zA-Z0-9À-ú]+', texto))
+print(re.findall(r'\w+', texto))
+
+# print(re.findall(r'[0-9]+', texto))
+print(re.findall(r'\d+', texto))
+
+print(re.findall(r'\S+', texto))
+
+print(re.findall(r'\bt\w+', texto, flags=re.I)) # begin
+print(re.findall(r'\w+da\b', texto, flags=re.I)) # tail
+print(re.findall(r'\b\w{7}\b', texto)) # linha 9
+print(re.findall(r'atual\w{2}\B', texto))
